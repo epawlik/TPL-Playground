08cd16b24c69d2d3c99d68f3966f3cad5d5bfbfd
epawlik <drew.pawlik@gmail.com>
2016-10-20 10:42:00 -0500

    Initial setup
    Created the initial WPF application project and a class library project.
    Added the TPL DataFlow package to the projects.

---
+++ TplPlayground/TplPlayground/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace TplPlayground
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
